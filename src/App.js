1060ad58bbfad8d978c98b97d24a1a059c867bfa
amy-u <amyung.lm@gmail.com>
2021-12-10 03:03:15 -0800

    Create Custom hook and used Fragment

--- src/App.js
+++ src/App.js
@@ -3,27 +3,34 @@ import React from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
-function App() {
+function useSemiPersistentState() {
   const [todoList, setTodoList] = React.useState(
-    JSON.parse(localStorage.getItem('savedTodoList')) || []
-    );
-    
-    React.useEffect(() => {
-      let stringTodoList = JSON.stringify(todoList);
-      localStorage.setItem('savedTodoList', stringTodoList);
-    }, [todoList]);
-    
-    const addTodo = (newTodo) => {
-      setTodoList([...todoList, newTodo]);
-    }
+    JSON.parse(localStorage.getItem("savedTodoList")) || []
+  );
+
+  React.useEffect(() => {
+    let stringTodoList = JSON.stringify(todoList);
+    localStorage.setItem("savedTodoList", stringTodoList);
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+};
+
+function App() {
+  const [todoList, setTodoList] = useSemiPersistentState();
+
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
+  };
+
   return (
-    <div>
+    <>
       <header>
         <h1>Todo List</h1>
       </header>
-      <AddTodoForm onAddTodo={addTodo}/>
+      <AddTodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
-    </div>
+    </>
   );
 }
 
