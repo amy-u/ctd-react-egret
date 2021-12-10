5cd5cab66a3bced1409c95a7a15109bb71dcce76
amy-u <amyung.lm@gmail.com>
2021-12-10 02:44:33 -0800

    Save Todo List in storage; stringify and parse data

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
 function App() {
-  const [todoList, setTodoList] = React.useState([]);
-  
-  const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo]);
-  }
-
-  React.useEffect(() => {
-    let stringTodoList = JSON.stringify(todoList);
-    localStorage.setItem('savedTodoList', stringTodoList);
-  });
-
+  const [todoList, setTodoList] = React.useState(
+    JSON.parse(localStorage.getItem('savedTodoList')) || []
+    );
+    
+    React.useEffect(() => {
+      let stringTodoList = JSON.stringify(todoList);
+      localStorage.setItem('savedTodoList', stringTodoList);
+    }, [todoList]);
+    
+    const addTodo = (newTodo) => {
+      setTodoList([...todoList, newTodo]);
+    }
   return (
     <div>
       <header>
